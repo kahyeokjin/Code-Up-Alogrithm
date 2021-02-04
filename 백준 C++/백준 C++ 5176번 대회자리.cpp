64d3a4f5aa6e71425c1b5ed5528903c7470fdf07
kahyeokjin <76166124+kahyeokjin@users.noreply.github.com>
2021-02-04 23:44:16 +0900

    Update 백준 C++ 5176번 대회자리.cpp

--- 백준 C++/백준 C++ 5176번 대회자리.cpp
+++ 백준 C++/백준 C++ 5176번 대회자리.cpp
@@ -9,18 +9,18 @@ int main() {
     {
         cin >> p >> m;
         int* a = new int[m];
-        for (int i = 0; i < m; i++) //제공되는 자리갯수만큼 0으로 지정
+        for (int i = 0; i < m; i++) 
             a[i] = 0;
         int ans = 0;
-        for (int i = 0; i < p; i++) //참가자들이 원하는 자리에 +1씩
+        for (int i = 0; i < p; i++) 
         {
             cin >> n;
             a[n - 1]++;
         }
-        for (int i = 0; i < m; i++) //자리수만큼 참여가능,ans는 참가못하는사람의수
+        for (int i = 0; i < m; i++) 
         {
             if (a[i] > 1)
-                ans += a[i] - 1; //참가할 -1
+                ans += a[i] - 1; 
         }
         b[c] = ans;
     }
