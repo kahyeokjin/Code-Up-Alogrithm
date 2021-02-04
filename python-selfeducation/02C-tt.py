4b66b13f17e8f4418899ccf930e9323fce7543c7
kahyeokjin <76166124+kahyeokjin@users.noreply.github.com>
2021-02-04 23:46:25 +0900

    Add files via upload

---
+++ python-selfeducation/02C-tt.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+t.color("red")
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+t.color("green")
+t.pensize(3)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+t.color("blue")
+t.pensize(5)
+t.circle(50)
