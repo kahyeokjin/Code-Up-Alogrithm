e0439901a01a184781649ca3a84e32a7831b08ab
kahyeokjin <76166124+kahyeokjin@users.noreply.github.com>
2021-02-04 23:16:41 +0900

    Add files via upload

---
+++ 02B-tt.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+t.circle(50)
