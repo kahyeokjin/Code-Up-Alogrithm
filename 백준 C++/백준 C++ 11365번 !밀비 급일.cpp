fb6a90ebd705ff4db6287c8e2bb4d03bc13ddec3
kahyeokjin <76166124+kahyeokjin@users.noreply.github.com>
2021-02-08 10:26:47 +0900

    Add files via upload

---
+++ 백준 C++/백준 C++ 11365번 !밀비 급일.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	while (1)
+	{
+		string s;
+		getline(cin, s);
+
+		if (s=="END")
+		{
+			break;
+		}
+
+		for (int i = (s.length() - 1); i >= 0; i--)
+		{
+			cout << s[i];
+		}
+		cout << '\n';
+	}
+	return 0;
+}
