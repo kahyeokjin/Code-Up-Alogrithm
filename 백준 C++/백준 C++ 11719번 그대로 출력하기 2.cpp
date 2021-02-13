4c5824131e49d4c51a40eac09b3a406cfda2c28e
kahyeokjin <76166124+kahyeokjin@users.noreply.github.com>
2021-02-13 16:59:15 +0900

    Add files via upload

---
+++ 백준 C++/백준 C++ 11719번 그대로 출력하기 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string list[100];
+	for (int i = 0; i < 100; i++)
+	{
+		getline(cin, list[i]);
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		cout << list[i] << endl;
+	}
+	return 0;
+}
